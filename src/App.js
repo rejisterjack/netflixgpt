3499a3c7c445849c38c2e899bb2309a275339ec0
Rupam Das <rejisterjack@gmail.com>
2024-10-15 21:03:53 +0530

    netflix gpt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Body from "./components/Body"
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <Body />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/f272782d-cf96-4988-a675-6db2afd165e0/web/IN-en-20241008-TRIFECTA-perspective_b28b640f-cee0-426b-ac3a-7c000d3b41b7_small.jpg"
+        alt=""
+      />
+      {/* 
+      start with login form: 55.00
+      */}
+    </div>
+  )
+}
+
+export default Login
